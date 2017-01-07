1ec38140e5aa390a6c4974e28daca9c3a022a301
Pat Rocchio <patrocc6@gmail.com>
2017-01-07 14:33:31 -0500

    Implemented creation process for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,35 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show]
+
   def index
 
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to @project, notice: 'Project created successfully'
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 end
--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -12,4 +12,23 @@ describe 'navigate' do
       expect(page).to have_content(/Projects/)
     end
   end
+
+  describe 'creation' do
+    before do
+      visit new_project_path
+    end
+
+    it 'has a new form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      fill_in 'project[name]', with: "New Project"
+      fill_in 'project[description]', with: "New project description"
+
+      click_on "Save"
+
+      expect(page).to have_content("New project description")
+    end
+  end
 end

9b2271949c4369b6991c6f28491bd7191dcf972e
Pat Rocchio <patrocc6@gmail.com>
2017-01-07 16:58:23 -0500

    Add projects index page

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -1,15 +1,30 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    user = User.create(email: "test@test.com", password: "asdfasdf",
+      password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached successfully' do
+    before do
       visit projects_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Projects' do
-      visit projects_path
       expect(page).to have_content(/Projects/)
+    end
+
+    it 'has a list of projects' do
+      project1 = Project.create(name: "Project 1", description: "test description")
+      project2 = Project.create(name: "Project 2", description: "test description")
+      visit projects_path
+      expect(page).to have_content(/Project 1|Project 2/)
     end
   end
 

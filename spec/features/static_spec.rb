d9f9b848245127a8de2442d4587234b536a2df7b
Pat Rocchio <patrocc6@gmail.com>
2017-01-26 17:54:53 -0500

    Add admin dashboard link to nav. Hide from non-admin users.

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user = FactoryGirl.create(:user)
+    login_as(@user, :scope => :user)
+  end
   describe 'homepage' do
     it 'can be reached successfully' do
       visit root_path
@@ -8,15 +12,19 @@ describe 'navigate' do
     end
 
     it 'hides navbar if not signed in' do
+      logout(:user)
       visit root_path
       expect(page).to_not have_content(/Home/)
     end
 
     it 'shows nav when user is signed in' do
-      @user = FactoryGirl.create(:user)
-      login_as(@user, :scope => :user)
       visit root_path
       expect(page).to have_content(/Home/)
+    end
+
+    it 'hides admin dashboard link for regular users' do
+      visit root_path
+      expect(page).to_not have_content(/Admin Dashboard/)
     end
   end
 end

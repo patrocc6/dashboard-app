7aa87398d1a56f54b28c23a7446297ed5316b7ca
Pat Rocchio <patrocc6@gmail.com>
2017-01-25 20:16:01 -0500

    Implemented hidden navbar when user is not signed in

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -6,5 +6,17 @@ describe 'navigate' do
       visit root_path
       expect(page.status_code).to eq(200)
     end
+
+    it 'hides navbar if not signed in' do
+      visit root_path
+      expect(page).to_not have_content(/Home/)
+    end
+
+    it 'shows nav when user is signed in' do
+      @user = FactoryGirl.create(:user)
+      login_as(@user, :scope => :user)
+      visit root_path
+      expect(page).to have_content(/Home/)
+    end
   end
 end

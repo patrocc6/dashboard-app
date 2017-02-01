bffd49bc33bee72fad18aca01faf310a5b2cf033
Pat Rocchio <patrocc6@gmail.com>
2017-02-01 18:05:05 -0500

    Add pagination to projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = Project.order(:name).page params[:page]
   end
 
   def new
